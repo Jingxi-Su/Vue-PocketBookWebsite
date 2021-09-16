87fd10e654abe5f3520568c212b04f867e4e1edb
jingxi <774874572@qq.com>
2021-09-17 07:44:50 +0800

    deploy

---
+++ precache-manifest.9c4d4743af5c7de7495f425642a0951e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44eed84e9f575f7316c9",
+    "url": "/Vue-PocketBookWebsite/css/app.4d4634ff.css"
+  },
+  {
+    "revision": "6060b6ddf32c710b8caa8c087a2510cf",
+    "url": "/Vue-PocketBookWebsite/index.html"
+  },
+  {
+    "revision": "44eed84e9f575f7316c9",
+    "url": "/Vue-PocketBookWebsite/js/app.376969d8.js"
+  },
+  {
+    "revision": "bac011e9a72852f22d65",
+    "url": "/Vue-PocketBookWebsite/js/chunk-vendors.bf2d3674.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/Vue-PocketBookWebsite/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-PocketBookWebsite/robots.txt"
+  }
+]);
